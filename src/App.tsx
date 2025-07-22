4c9fb55b82d4f49574a662f84c834ae63d0956b1
Ibra-SPb <Ibrahim-1991@mail.ru>
2025-07-22 11:45:00 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-xl font-montserrat">My fitness project</h1>
+    </div>
+  );
+}
+
+export default App;
